12b47345a27d35791b2363cfc48eb2389050b9ab
renoker <rodolfoulises.ramirez@gmail.com>
2025-02-14 15:57:45 -0600

    Update

--- resources/views/pages/orden/index.blade.php
+++ resources/views/pages/orden/index.blade.php
@@ -40,28 +40,6 @@
 
 
         document.addEventListener('alpine:init', () => {
-            // main section
-            Alpine.data('scrollToTop', () => ({
-                showTopButton: false,
-                init() {
-                    window.onscroll = () => {
-                        this.scrollFunction();
-                    };
-                },
-
-                scrollFunction() {
-                    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-                        this.showTopButton = true;
-                    } else {
-                        this.showTopButton = false;
-                    }
-                },
-
-                goToTop() {
-                    document.body.scrollTop = 0;
-                    document.documentElement.scrollTop = 0;
-                },
-            }));
 
             // theme customization
             Alpine.data('customizer', () => ({
